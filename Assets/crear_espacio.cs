6b6086d7ff5cb274b435ea519193475fde8f9be5
Gary <gortiz1490@gmail.com>
2021-04-29 14:51:21 -0600

    manejo de validaciones 3/5

--- Assets/crear_espacio.cs
+++ Assets/crear_espacio.cs
@@ -95,34 +95,59 @@ public class crear_espacio : MonoBehaviour
     }
     public void mueble_posicion_change(Dropdown sender) {
         if (sender.name == "drop2") {
-            muebles.Add(sender.options[sender.value].text, "silla");
-            esp.silla = sender.options[sender.value].text;
-
-
+            if (Ingresar_muebles_a_espacio(sender.options[sender.value].text, "silla")) {
+                esp.silla = sender.options[sender.value].text;
+            }
         }
         else if (sender.name == "drop3")
         {
-            muebles.Add(sender.options[sender.value].text, "mesa");
-            esp.mesa = sender.options[sender.value].text;
+
+            if (Ingresar_muebles_a_espacio(sender.options[sender.value].text, "mesa"))
+            {
+                esp.mesa = sender.options[sender.value].text;
+            }
 
         }
         else if (sender.name == "drop4")
         {
-            muebles.Add(sender.options[sender.value].text, "gabinete");
-            esp.gabinete = sender.options[sender.value].text;
+            if (Ingresar_muebles_a_espacio(sender.options[sender.value].text, "gabinete"))
+            {
+                esp.gabinete = sender.options[sender.value].text;
+            }
 
         }
         else if (sender.name == "drop5")
         {
-            muebles.Add(sender.options[sender.value].text, "comoda");
-            esp.comoda = sender.options[sender.value].text;
+
+            if (Ingresar_muebles_a_espacio(sender.options[sender.value].text, "comoda"))
+            {
+                esp.comoda = sender.options[sender.value].text;
+            }
 
         }
         else
          if (sender.name == "drop6")
         {
-            muebles.Add(sender.options[sender.value].text, "banco");
-            esp.banco = sender.options[sender.value].text;
+            if (Ingresar_muebles_a_espacio(sender.options[sender.value].text, "banco"))
+            {
+                esp.banco = sender.options[sender.value].text;
+            }
+
+        }
+    }
+    //metodo que ingresa los muebles y donde vamos aver que no se repitan los muebles en el espacio
+    public bool Ingresar_muebles_a_espacio(string posicion, string mueble) {
+        if (!muebles.ContainsKey(posicion))
+        {
+            muebles.Add(posicion, mueble);
+            return true;
+        }
+        else {
+            if (EditorUtility.DisplayDialog("ERROR 0002", "\n¡¡¡¡Ya existe este mueble en otra posicion!!!!", "Ok"))
+            {
+                return false;
+            }
+            return false;
 
         }
     }
@@ -137,21 +162,28 @@ public class crear_espacio : MonoBehaviour
     //inicializar de espacios
     
     public void mandardatos() {
+        
         GameStatus.insertar_lista("imagen_" + GameStatus.contador_espacio.ToString(), GameStatus.piso);
         GameStatus.contador_espacio++;
         GameStatus.insertar_lista_espacios(esp);
-        GameStatus.insertar_lista_imagen_pos_muebles(esp.imagen, muebles);
 
-        if (EditorUtility.DisplayDialog("EXITO!!", "Escenario Creado!!\n¿Desea crea otro objeto o regresar al menu principal?", "Crear otro", "Menu principal"))
-        {
-            //aqui agregamos al diccionario el espacio con su imagen
-            
-            SceneManager.LoadScene("crear_espacio");
-        }
-        else
-        {
-            SceneManager.LoadScene("MainMenu");
-        }
+        //No se podrá crear más de 6 espacios debido a la limitación de los pisos
+        //metodo dentro del gamestatus para ver si hay mas de 6 espacios si lo hay-- tira error
 
+        //aqui tambien ingresamos los espacios a la lista global de espacios
+        if (GameStatus.Insertar_lista_imagen_pos_muebles(esp.imagen, muebles))
+        {
+
+            if (EditorUtility.DisplayDialog("EXITO!!", "Escenario Creado!!\n¿Desea crea otro objeto o regresar al menu principal?", "Crear otro", "Menu principal"))
+            {
+                //aqui agregamos al diccionario el espacio con su imagen
+
+                SceneManager.LoadScene("crear_espacio");
+            }
+            else
+            {
+                SceneManager.LoadScene("MainMenu");
+            }
+        }
     }
 }
